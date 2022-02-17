fb47ac19465a5a4120c3cb3a2284dc44bea9eab7
Vinoo <ivinoop@gmail.com>
2022-02-17 23:16:55 +0530

    v1.0

---
+++ index.js
@@ -0,0 +1,46 @@
+const url = 'https://dev.to/api/articles?username=ivinoop';
+
+let rootElm = document.querySelector('.blog-container');
+let displayBlogs = document.querySelector('.display-blogs');
+
+function createUI(blogs) {
+  rootElm.innerHTML = '';
+  blogs.forEach((blog) => {
+
+    let blogContent = document.createElement('li');
+    // blogContent.classList.add('.blog-content');
+
+    let blogImg = document.createElement('img');
+    blogImg.src = blog.social_image;
+
+    // let blogText = document.createElement('div');
+    // blogText.classList.add('blog-text');
+
+    let blogTitle = document.createElement('h2');
+    blogTitle.innerText = blog.title;
+
+    let blogDescription = document.createElement('p');
+    blogDescription.innerText = blog.description;
+
+    let readMore = document.createElement('a');
+    readMore.href = blog.url;
+    readMore.target = '_blank';
+    readMore.innerText = 'Read Article →';
+
+    blogContent.append(blogImg, blogTitle, blogDescription, readMore);
+
+    // blogContent.append(blogImg, blogText);
+    rootElm.append(blogContent);
+    displayBlogs.append(rootElm);
+  });
+} 
+
+function fetchData() {
+  fetch(url)
+  .then((res) => res.json())
+  .then((blogData) => {
+    createUI(blogData);
+  }) 
+}
+
+fetchData();
