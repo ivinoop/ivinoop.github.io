c5fa77f7a30da007e38b9034107730be429fa5c3
Vinoo <ivinoop@gmail.com>
2022-02-25 02:20:16 +0530

    fixed mobile menu page appearance

--- banner.js
+++
@@ -1,8 +0,0 @@
-let banner = document.querySelector('.banner');
-let closeBtn = document.querySelector('.popup-close');
-
-function handlePopup() {
-  banner.style.display = 'none';
-}
-
-closeBtn.addEventListener('click', handlePopup);
